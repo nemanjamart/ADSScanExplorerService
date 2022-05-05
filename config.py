1818b9db6cce880bd453f369c4fe5b08d2abd2e7
Philip Matsson <philip@winterway.eu>
2022-05-05 14:36:13 +0200

    Switched to postgresql and updated model

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ IMAGE_API_SERVER = 'http://localhost:8182'
 IMAGE_API_BASE_PATH = '/iiif/2'
 IMAGE_API_BASE_URL = f'{IMAGE_API_SERVER}{IMAGE_API_BASE_PATH}'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+SQLALCHEMY_DATABASE_URI = 'postgres://scan_explorer:scan_explorer@localhost:5432/scan_explorer_pipeline'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- scan_explorer_service/models.py
+++ scan_explorer_service/models.py
@@ -1,45 +1,96 @@
+import uuid
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String, Table, UniqueConstraint
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, String, Table, UniqueConstraint, Enum
+from sqlalchemy.orm import relationship, Session
+from sqlalchemy_utils.types import UUIDType
+import enum
 
 Base = declarative_base()
+
+
+class VolumeStatus(enum.Enum):
+    """Volume ingestion status"""
+    New = 1
+    Processing = 2
+    Update = 3
+    Done = 4
+    Error = 5
+
+
+class PageColor(enum.Enum):
+    """Page Color Type"""
+    BW = 1
+    Greyscale = 2
+    Color = 3
+
+
+class PageType(enum.Enum):
+    """Page Type."""
+    Normal = 1
+    FrontMatter = 2
+    BackMatter = 3
+    Insert = 4
+    Plate = 5
 
 
 class JournalVolume(Base):
     __tablename__ = 'journal_volume'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    def __init__(self, type, journal, volume):
+        self.type = type
+        self.journal = journal
+        self.volume = volume
+
+    id = Column(UUIDType, default=uuid.uuid4, primary_key=True)
     journal = Column(String)
     volume = Column(String)
     type = Column(String)
+    status = Column(Enum(VolumeStatus))
+    file_hash = Column(String)
 
 
 page_article_association_table = Table('page2article', Base.metadata,
-    Column('page_id', ForeignKey('page.id'), primary_key=True),
-    Column('article_id', ForeignKey('article.id'), primary_key=True)
-)
+                                       Column('page_id', ForeignKey(
+                                           'page.id'), primary_key=True),
+                                       Column('article_id', ForeignKey(
+                                           'article.id'), primary_key=True)
+                                       )
+
 
 class Article(Base):
     __tablename__ = 'article'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    def __init__(self, bibcode, journal_volume_id):
+        self.bibcode = bibcode
+        self.journal_volume_id = journal_volume_id
+
+    id = Column(UUIDType, default=uuid.uuid4, primary_key=True)
     bibcode = Column(String)
-    journal_volume_id = Column(Integer, ForeignKey(JournalVolume.id))
-    pages = relationship('Page', secondary=page_article_association_table, back_populates='articles', lazy='dynamic')
+    journal_volume_id = Column(UUIDType, ForeignKey(JournalVolume.id))
+    pages = relationship('Page', secondary=page_article_association_table,
+                         back_populates='articles', lazy='dynamic')
+
 
 class Page(Base):
     __tablename__ = 'page'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    def __init__(self, name, journal_volume_id):
+        self.name = name
+        self.journal_volume_id = journal_volume_id
+        self.color_type = PageColor.BW
+        self.parse_info_from_name(name)
+
+    id = Column(UUIDType, default=uuid.uuid4,  primary_key=True)
     name = Column(String)
     label = Column(String)
     format = Column(String, default='image/tiff')
-    color_type = Column(String)
-    page_type = Column(String)
+    color_type = Column(Enum(PageColor))
+    page_type = Column(Enum(PageType))
     width = Column(Integer)
     height = Column(Integer)
-    journal_volume_id = Column(Integer, ForeignKey(JournalVolume.id))
+    journal_volume_id = Column(UUIDType, ForeignKey(JournalVolume.id))
     volume_running_page_num = Column(Integer)
-    articles = relationship('Article', secondary=page_article_association_table, back_populates='pages')
+    articles = relationship(
+        'Article', secondary=page_article_association_table, back_populates='pages')
 
     UniqueConstraint(journal_volume_id, volume_running_page_num)
