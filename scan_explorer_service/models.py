d2a0b5e0c2b9813291a9073b3d872dc602b8caa3
mattiasw <mattias.wangblad@gmail.com>
2022-05-17 14:02:47 +0200

    Updated models to match with pipeline

--- scan_explorer_service/models.py
+++ scan_explorer_service/models.py
@@ -1,9 +1,10 @@
 import uuid
 from flask import current_app
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String, Table, UniqueConstraint, Enum
-from sqlalchemy.orm import relationship, Session
+from sqlalchemy import Column, ForeignKey, Integer, String, Table, UniqueConstraint, Enum, Index
+from sqlalchemy.orm import relationship
 from sqlalchemy_utils.types import UUIDType
+from sqlalchemy_utils.models import Timestamp
 import enum
 
 Base = declarative_base()
@@ -14,16 +15,16 @@ class VolumeStatus(enum.Enum):
     New = 1
     Processing = 2
     Update = 3
-    Done = 4
-    Error = 5
-
+    Db_done = 4
+    Bucket_done = 5
+    Done = 6
+    Error = 7
 
 class PageColor(enum.Enum):
     """Page Color Type"""
     BW = 1
     Greyscale = 2
     Color = 3
-
 
 class PageType(enum.Enum):
     """Page Type."""
@@ -33,23 +34,27 @@ class PageType(enum.Enum):
     Insert = 4
     Plate = 5
 
-
-class JournalVolume(Base):
-    __tablename__ = 'journal_volume'
-
+class JournalVolume(Base, Timestamp):
+    
     def __init__(self, type, journal, volume):
         self.type = type
         self.journal = journal
         self.volume = volume
+
+    __tablename__ = 'journal_volume'
+    __table_args__ = (Index('volume_index', "journal", "volume"), )
 
     id = Column(UUIDType, default=uuid.uuid4, primary_key=True)
     journal = Column(String)
     volume = Column(String)
     type = Column(String)
     status = Column(Enum(VolumeStatus))
+    status_message = Column(String)
     file_hash = Column(String)
     pages = relationship(
-        'Page', primaryjoin='JournalVolume.id==Page.journal_volume_id', back_populates='volume')
+        'Page', primaryjoin='JournalVolume.id==Page.journal_volume_id', back_populates='volume', order_by="Page.volume_running_page_num")
+
+    UniqueConstraint(journal, volume)
 
 
 page_article_association_table = Table('page2article', Base.metadata,
@@ -60,8 +65,10 @@ page_article_association_table = Table('page2article', Base.metadata,
                                        )
 
 
-class Article(Base):
+class Article(Base, Timestamp):
     __tablename__ = 'article'
+    __table_args__ = (Index('article_volume_index', "journal_volume_id"), Index('article_bibcode_index', "bibcode"))
+
 
     def __init__(self, bibcode, journal_volume_id):
         self.bibcode = bibcode
@@ -71,7 +78,7 @@ class Article(Base):
     bibcode = Column(String)
     journal_volume_id = Column(UUIDType, ForeignKey(JournalVolume.id))
     pages = relationship('Page', secondary=page_article_association_table,
-                         back_populates='articles', lazy='dynamic')
+                         back_populates='articles', lazy='dynamic', order_by="Page.volume_running_page_num")
 
     @property
     def serialized(self):
@@ -84,8 +91,9 @@ class Article(Base):
         }
 
 
-class Page(Base):
+class Page(Base, Timestamp):
     __tablename__ = 'page'
+    __table_args__ = (Index('page_volume_index', "journal_volume_id"), Index('page_name_index', "name"))
 
     def __init__(self, name, journal_volume_id):
         self.name = name
@@ -107,6 +115,7 @@ class Page(Base):
 
     volume = relationship('JournalVolume', back_populates='pages')
     UniqueConstraint(journal_volume_id, volume_running_page_num)
+    UniqueConstraint(journal_volume_id, name)
 
     @property
     def image_url(self):
