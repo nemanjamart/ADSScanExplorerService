fcf11982279ff14af905ec98edecc46145c2e18a
Philip Matsson <philip@winterway.eu>
2022-06-27 17:24:41 +0200

    Enabled wsgi  multithreading

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,5 @@ from scan_explorer_service import app as application
 if __name__ == "__main__":
 
     run_simple(
-        '0.0.0.0', 8181, application, use_reloader=False, use_debugger=True
+        '0.0.0.0', 8181, application, use_reloader=False, use_debugger=True, threaded=True
     )
